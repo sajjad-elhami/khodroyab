4b90579ef32b33d17233475d060ad57abf41863c
sajjad-elhami <elhami.icloud@gmail.com>
2026-09-11 19:24:52 +0330

    fix: allow current LAN dev origin for mobile testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["172.20.10.3"],
+  allowedDevOrigins: ["172.20.10.3", "172.16.20.35"],
   /* config options here */
   reactCompiler: true,
   devIndicators: false,
